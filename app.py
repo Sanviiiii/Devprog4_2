9bdef0d21e8417b1f5f1dde289f48aacbca9e559
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-05-26 00:53:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8095)
+#kubectl apply -f pod.yaml
+#kubectl get pods
+#kubectl describe pod my-nginx
+#kubectl get pods -o wide
+#minikube ssh -it will login into the minikube cluster  
+#curl  <nginx ip-address> 
